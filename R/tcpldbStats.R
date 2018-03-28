10b453dd574f7edd74640c85b2511c87d07f9a59
spendse <spendse@scitovation.com>
2018-03-28 17:16:21 -0400

    Tox-33 now complete. New mc0 and mc4 files added for testing tox-33.

--- R/tcpldbStats.R
+++ R/tcpldbStats.R
@@ -2,19 +2,43 @@
 # Generate summary stats for the table
 #----------------------------------------------------------------------------------
 
+#' @title Get summary statisitcs for the database
+#' 
+#' @description 
+#' \code{tcpldbStats} takes a string(type) and an optional parameter(val) to return the summary statistics on the entire tcplLite database
+#'  When type = "all" the val is ignored. the function returns the number of distinct spid and aeids in the database at each level
+#'  When type = "aeid", the val parameter has to be a valid aeid in the database. The function returns a 
+#'  table consisting of the number of distinct spids at each level of processing for the aeid given in 'val'
+#'  When type = "spid", the val parameter has to be a valid spid in the database. The function returns a 
+#'  table consisiting of the number of distinct aeids at each level of processing for the given spid in 'val'
+#' 
+#' @param type String either "all", "aeid" or "spid"
+#' @param val interger if type = "aeid" , string if type  = "spid"
+#' @export
+
 tcpldbStats <- function(type = "all", val = NULL){
   # get a list of acid to number of aeids for each acid
   query <- "Select acid,Count(aeid) as 'num_aeids' from assay_component_endpoint Group By acid;"
   acid_aeid_list <- tcplQuery(query,tbl = "assay_component_endpoint")
   acid_aeid_list <- setNames(acid_aeid_list$num_aeids,acid_aeid_list$acid)
-  #Get a list of spids with the m0ids as key
-  query <- "Select spid, m0id from mc0;"
-  m0id_spid_list <- tcplQuery(query,tbl = "mc0")
-  m0id_spid_list <- setNames(m0id_spid_list$spid,m0id_spid_list$m0id)
+  # get the list of m0ids for each spid at mc0
+  query <- "select spid, group_concat(m0id) as 'm0id_list' from mc0 group by spid;"
+  spid_mc0_lst <- tcplQuery(query,tbl = "mc0")
+  spid_mc0_lst <- setNames(spid_mc0_lst$m0id_list,spid_mc0_lst$spid)
+  # get the list of m4ids for each spid at mc4
+  query <- "select spid, group_concat(m4id) as 'm4id_list' from mc4 group by spid;"
+  spid_mc4_lst <- tcplQuery(query,tbl = "mc4")
+  spid_mc4_lst <- setNames(spid_mc4_lst$m4id_list,spid_mc4_lst$spid)
+  
+  # #Get a list of spids with the m0ids as key
+  # query <- "Select spid, m0id from mc0;"
+  # m0id_spid_list <- tcplQuery(query,tbl = "mc0")
+  # m0id_spid_list <- setNames(m0id_spid_list$spid,m0id_spid_list$m0id)
+  num_spid <- list()
+  num_aeid <- list()
   if (type == "all"){
     # Table 1 Number of distinct spid and aeid per processing level
-    num_spid <- list()
-    num_aeid <- list()
+   
    
     
     ##SPIDS per processing level
@@ -37,7 +61,7 @@ tcpldbStats <- function(type = "all", val = NULL){
       mc_tble <- paste0("mc",x)
       query <- sprintf("Select count(distinct spid) as 'spid' From mc4 inner join %s on mc4.m4id = %s.m4id;",
                        mc_tble,mc_tble)
-      val <- tcplQuery(query, tble = c("mc4",mc_tble))
+      val <- tcplQuery(query, tbl = c("mc4",mc_tble))
       num_spid <- c(num_spid,val$spid)
     }
     #Aeids per processing level
@@ -56,34 +80,68 @@ tcpldbStats <- function(type = "all", val = NULL){
       val <- tcplQuery(query, tbl = c("assay_component_endpoint",mc_tble))
       num_aeid <- c(num_aeid,val$num_aeid)
     }
-    tble1 <- data.frame("Levels"=c("mc0","mc1","mc2","mc3","mc4","mc5","mc6"),
+    tble <- data.frame("Levels"=c("mc0","mc1","mc2","mc3","mc4","mc5","mc6"),
                         "spid" = num_spid,
                         "aeid" = num_aeid,
                         stringsAsFactors = F)
-  }else if(type == "aeid"){
-    aeid <- val
-    # find the acid associated with the aeid to use in L0 to L2
-    acid <- as.numeric(tcplQuery(sprintf("Select acid from assay_component_endpoint where aeid = %f;",aeid),
-                      tbl = "assay_component_endpoint")$acid)
-    
   }
-  
-  
-  
-  
   ## Table 2 : for a given aeid, how many spids are present at each level
-  
-  
-  query <- "Select count(distinct aeid) as 'aeid' From assay_component_endpoint inner join mc0 on mc0.acid = assay_component_endpoint.acid"
-  num_aeid <- c(num_aeid,tcplQuery(query,tbl = c("assay_component_endpoint","mc0"))$aeid)
-  # get all m0ids at L1 
-  query <- "Select distinct m0id from mc1;"
-  L1m0 <- tcplQuery(query, tbl = "mc1")
-  # get all m0ids at L2
-  query <- "Select distinct m0id from mc2;"
-  L2m0 <- tcplQuery(query, tbl = "mc2")
-  # get all m0ids at L3
-  query <- "Select distinct m0id from mc3;"
-  L3m0 <- tcplQuery(query, tbl = "mc3")
-  
+  else if(type == "aeid"){
+    aeid <- val
+    # find the acid associated with the aeid to use in L0 to L3
+    acid <- as.numeric(tcplQuery(sprintf("Select acid from assay_component_endpoint where aeid = %d;",aeid),
+                      tbl = "assay_component_endpoint")$acid)
+    query <- sprintf("Select count(distinct spid) as 'num_spid' from mc0 where mc0.acid = %d", acid)
+    num_spid <- c(num_spid, tcplQuery(query, tbl= "mc0")$num_spid)
+    # for level 1 to level 3, find number of distinct spid for a given acid
+    for (x in c(1:3)){
+      mc_tble <- paste0("mc",x)
+      query <- sprintf("Select count(distinct spid) as 'spid' From mc0 inner join %s on mc0.m0id = %s.m0id where %s.acid = %d;",
+                       mc_tble,mc_tble,mc_tble,acid)
+      val <- tcplQuery(query, tbl = c("mc0",mc_tble))
+      num_spid <- c(num_spid,val$spid)
+    }
+    # for L4
+    query <- sprintf("Select count(distinct spid) as 'num_spid' from mc4 where mc4.aeid = %d", aeid)
+    num_spid <- c(num_spid, tcplQuery(query, tbl= "mc4")$num_spid)
+    # for L5 to L6
+    for (x in c(5:6)){
+      mc_tble <- paste0("mc",x)
+      query <- sprintf("Select count(distinct spid) as 'spid' From mc4 inner join %s on mc4.m4id = %s.m4id where %s.aeid = %d;",
+                       mc_tble,mc_tble,mc_tble,aeid)
+      val <- tcplQuery(query, tbl = c("mc0",mc_tble))
+      num_spid <- c(num_spid,val$spid)
+    }
+    tble <- data.frame("Levels"=c("mc0","mc1","mc2","mc3","mc4","mc5","mc6"),
+                        "spid" = num_spid,
+                        "aeid" = rep(val,7),
+                        stringsAsFactors = F)
+  }
+  ## Table 3 : for a given spid, how many aeids are present at each level
+  else if (type == "spid"){
+    # find m0ids for the given spid
+    m0id_list <- spid_mc0_lst[val]
+    # find m0ids for the given spid
+    m4id_list <- spid_mc4_lst[val]
+    # for level 0 through 3, find the list of distinct acid that serve these m0ids
+    for(x in c(0:3)){
+      mc_tble <- paste0("mc",x)
+      query <- sprintf("Select distinct acid from %s where %s.m0id in (%s);",mc_tble,mc_tble,m0id_list)
+      val <- tcplQuery(query, tbl = mc_tble)
+      num_aeid <- c(num_aeid,sum(acid_aeid_list[val$acid]))
+    }
+    # for level 4 through 6, find the list of distinct aeid that serve these m4ids
+    for(x in c(4:6)){
+      mc_tble <- paste0("mc",x)
+      query <- sprintf("Select count(distinct aeid) as 'num_aeid' from %s where %s.m4id in (%s);",mc_tble,mc_tble,m4id_list)
+      val <- tcplQuery(query, tbl = mc_tble)
+      num_aeid <- c(num_aeid,sum(val$num_aeid))
+    }
+    tble <- data.frame("Levels"=c("mc0","mc1","mc2","mc3","mc4","mc5","mc6"),
+                       "aeid" = num_aeid,
+                       "spid" = rep(val,7),
+                       stringsAsFactors = F)
+     
+  }
+  return(tble)
 }
