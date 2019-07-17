f5594c2a51d8c4610713de453befb04aa48e77da
jbrown20 <brown.jason@epa.gov>
2019-07-17 10:56:55 -0400

    added dat function call similar to tcplLoadData and lvl check similar to tcplPlotM4id

--- R/tcplPlot.R
+++ R/tcplPlot.R
@@ -1,5 +1,5 @@
 #'#-------------------------------------------------------------------------------
-# tcplLoadData: Load tcpl data
+# tcplPlot: plot tcpl data
 #-------------------------------------------------------------------------------
 
 #'  Generic Plotting Function for tcpl
@@ -32,6 +32,23 @@
 #' @examples
 tcplPlot <- function(lvl = 4, fld = NULL, val = NULL, type = "mc") {
   
+  if (length(lvl) > 1 | !lvl %in% 4:7) stop("invalid lvl input.")
   
+  prs <- list(type = "mc", fld = fld, val = val)
+  
+  if (lvl == 4L) dat <- do.call(tcplLoadData, args = c(lvl = 4L, prs))
+  if (lvl >= 5L) dat <- do.call(tcplLoadData, args = c(lvl = 5L, prs))
+  if (lvl >= 6L) {
+    flg <- do.call(tcplLoadData, args = c(lvl = 6L, prs))
+  } else {
+    flg <- NULL
+  }
+  if (lvl == 7L) {
+    boot <- do.call(tcplLoadData, args = c(lvl = 7L, prs))
+  } else {
+    boot <- NULL
+  }
+  
+  dat
   
 }
