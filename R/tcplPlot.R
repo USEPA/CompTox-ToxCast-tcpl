8b43e820e703730ed3f1ae15fc1c123572ea2757
jbrown20 <brown.jason@epa.gov>
2019-07-17 10:46:11 -0400

    Initial commit for updated plotting

---
+++ R/tcplPlot.R
@@ -0,0 +1,37 @@
+#'#-------------------------------------------------------------------------------
+# tcplLoadData: Load tcpl data
+#-------------------------------------------------------------------------------
+
+#'  Generic Plotting Function for tcpl
+#'
+#' @description
+#' \code{tcplLoadData} queries the tcpl databases and returns a plot
+#' for the given level and data type.
+#'
+#' @param lvl Integer of length 1, the level of data to load
+#' @param type Character of length 1, the data type, "sc" or "mc"
+#' @param fld Character, the field(s) to query on
+#' @param val List, vectors of values for each field to query on. Must be in
+#' the same order as 'fld'.
+#'
+#' @details
+#' The data type can be either 'mc' for mutliple concentration data, or 'sc'
+#' for single concentration data. Multiple concentration data will be loaded
+#' into the 'mc' tables, whereas the single concentration will be loaded into
+#' the 'sc' tables.
+#'
+#' Setting 'lvl' to "agg" will return an aggregate table containing the m4id
+#' with the concentration-response data and m3id to map back to well-level
+#' information.
+#'
+#' Leaving \code{fld} NULL will return all data.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tcplPlot <- function(lvl = 4, fld = NULL, val = NULL, type = "mc") {
+  
+  
+  
+}
