2a5f2bca9f07c886dd73edcd2644803abae03d30
jbrown20 <brown.jason@epa.gov>
2019-10-30 10:08:03 -0400

    first working commit of function for api spid call

--- R/tcplspidAPIsearch.R
+++ R/tcplspidAPIsearch.R
@@ -1,4 +1,16 @@
-tcplspidAPIsearch <- function(spid){
+#' A function for hitting the chemtrack api to get sample details
+#'
+#' @param spid single spid or an array of spids
+#' @param verbose if true we want to return all values from api, not just those that match the sample table format
+#'
+#' @return a data_table containing the results of hitting the chemtrack api for each spid
+#'
+#' @examples 
+#' tcpl:::tcplspidAPIsearch("PINTO01C04")
+tcplspidAPIsearch <- function(spid, verbose = FALSE){
+  #variable binding
+  errors <- NULL
+  
   # check that we have one or more spids
   
   # URL we are hitting to get the chemtrack results
@@ -12,28 +24,39 @@ tcplspidAPIsearch <- function(spid){
   # need to add 100 ms delay
   df <- lapply(spid, function(x) {
     Sys.sleep(.1)
-    httr::content(GET(paste0(baseurl,x)))}
+    httr::content(httr::GET(paste0(baseurl,x)))}
   )
   
+  df <- dplyr::bind_rows(df)
+  df$spid <- spid
+  
   #convert raw chemtrack to sample table format
-  spid_add <- df %>%
-    {
-      tibble(
-        spid = spid,
-        stkc = map_dbl(., "sample_concentration"),
-        stkc_unit = map_chr(., "sample_concentration_unit"),
-        dsstox_substance_id = map_chr(., "dtxsid")
-      )
-    } %>%
-    mutate(tested_conc_unit = case_when(
-      stkc_unit == "mM" ~ "uM",
-      stkc_unit == "mg/ml" ~ "mg/l",
-      stkc_unit == "kCF" ~ "CF"
-    )) %>%
-    rowwise() %>%
-    mutate(chid = unlist(tcplQuery(paste0("SELECT id FROM ro_stg_dsstox.generic_substances where dsstox_substance_id = '", dsstox_substance_id, "';"),
-                                   db = "ro_stg_dsstox",
-                                   tbl = "generic_substances"))) %>% 
-    select(spid,chid,stkc,stkc_unit,tested_conc_unit)
+  if("sample_concentration_unit" %in% names(df)){
+  df <- dplyr::mutate(df, tested_conc_unit = dplyr::case_when(
+      sample_concentration_unit == "mM" ~ "uM",
+      sample_concentration_unit == "mg/ml" ~ "mg/l",
+      sample_concentration_unit == "kCF" ~ "CF"
+    ))
+  }
+  
+  if("errors" %in% names(df)){
+    df$errors <- unlist(tidyr::replace_na(df$errors,NA))
+    df <- dplyr::mutate(df,errors = unlist(errors))
+  }
+  
+  df <- dplyr::select(df,spid,dplyr::everything())
+  
+  # warn if errors or not unblinded
+  if("errors" %in% names(df)){
+    errs <- unique(unlist(df$errors))[!is.na(unique(unlist(df$errors)))]
+    for(e in errs){
+      total_err <- sum(e == df$errors, na.rm = TRUE)
+      warning(paste0(total_err, " spid(s) returned the following error: ", e))
+    }
+  }
+
+  #could do some formatting here to match sample table in invitrodb  
+  df
+
   
 }
