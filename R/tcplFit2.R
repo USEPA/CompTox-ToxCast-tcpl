165ff2ad25ab1a11f25385d9a20e34eaf46d32b1
jbrown20 <brown.jason@epa.gov>
2020-01-09 12:04:04 -0500

    added fit2_core fitting

--- R/tcplFit2.R
+++ R/tcplFit2.R
@@ -3,11 +3,11 @@
 #' @param dat
 #'
 #' @return
-#'
+#' @importFrom tcplfit2 tcplfit2_core
 #' @examples
 tcplFit2 <- function(dat) {
   # do all the regular fitting things that still need to be done
-  dat <- copy(dat)[, `:=`(c("rmns", "rmds", "nconcs", "med_rmds"), {
+  res <- dat[, `:=`(c("rmns", "rmds", "nconcs", "med_rmds"), {
     rmns <- mean(resp)
     rmds <- median(resp)
     nconcs <- .N
@@ -19,15 +19,17 @@ tcplFit2 <- function(dat) {
     max_med = max(rmds), max_med_conc = logc[which.max(rmds)],
     logc_max = max(logc), logc_min = min(logc), nconc = length(unique(logc)),
     npts = .N, nrep = median(as.numeric(nconcs)), nmed_gtbl = sum(med_rmds) / first(nconcs),
-    concentration = list(logc), response = list(resp)
-  ), keyby = .(
-    aeid,
-    spid
-  )]
+    concentration_unlogged = list(exp(logc)), response = list(resp)
+  ),
+  keyby = .(aeid, spid)
+  ][, `:=`(tmpi = seq_len(.N)), keyby = .(aeid)][,
+    `:=`(fitparams = list(tcplfit2::tcplfit2_core(unlist(concentration_unlogged),
+      unlist(response), 0,
+      verbose = FALSE, force.fit = TRUE
+    ))),
+    keyby = .(spid)
+  ]
 
-  # fit with tcplfit2_core per spid
-  output <- tcplfit2_core(conc, resp, 0, force.fit = TRUE)
-
-  # return wide format dat
-  dat
+  # return dat with fitted curves in fitparams
+  res
 }
