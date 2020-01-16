35ecbee036dd6c71c48922478d89e1e5f306e779
jbrown20 <brown.jason@epa.gov>
2020-01-16 10:34:12 -0500

    updated delete cascade

--- R/tcplCascade.R
+++ R/tcplCascade.R
@@ -50,6 +50,7 @@ tcplCascade <- function(lvl, type, id) {
     if (lvl <= 3) tcplDelete(tbl = "mc3", fld = "aeid", val = id, db = db)
     if (lvl <= 4) tcplDelete(tbl = "mc4", fld = "aeid", val = id, db = db)
     if (lvl <= 4) tcplDelete(tbl = "mc4_agg", fld = "aeid", val = id, db = db)
+    if (lvl <= 4  && check_tcpl_db_schema()) tcplDelete(tbl = "mc4_param", fld = "aeid", val = id, db = db)
     if (lvl <= 5) tcplDelete(tbl = "mc5", fld = "aeid", val = id, db = db)
     if (lvl <= 6) tcplDelete(tbl = "mc6", fld = "aeid", val = id, db = db)
     
