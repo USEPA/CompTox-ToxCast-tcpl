59d4f458ecb6be327d46ac7322f434f23790890f
Dunham <Dunham.Kurt@epa.gov>
2023-03-06 16:45:56 -0500

    changed scale_color_viridis_d toscale_color_manual, used color values from running viridis::viridis(5,end=0.9), changed bmd and bmr lines to segments that stop at the curve fit, and set bmd line to solid and same color as bmr line

--- R/tcplPlot.R
+++ R/tcplPlot.R
@@ -620,13 +620,15 @@ tcplggplot <- function(dat, lvl = 5, verbose = FALSE) {
     geom_function(aes(color = !!model_test("pow"), linetype = !!model_test("pow")), fun = function(x) tcplfit2::pow(ps = c(dat$pow_a, dat$pow_p), x = x)) +
     geom_function(aes(color = !!model_test("hill"), linetype = !!model_test("hill")), fun = function(x) tcplfit2::hillfn(ps = c(dat$hill_tp, dat$hill_ga, dat$hill_p), x = x)) +
     geom_vline(aes(xintercept = dat$ac50, color = "AC50", linetype = "AC50")) +
-    geom_vline(aes(xintercept = dat$bmd, color = "BMD", linetype = "BMD")) +
+    geom_segment(aes(x=dat$bmd, xend=dat$bmd, y=-Inf, yend=dat$bmr, color = "BMD", linetype = "BMD")) +
     geom_hline(aes(yintercept = dat$coff, color = "Cutoff", linetype = "Cutoff")) +
-    geom_hline(aes(yintercept = dat$bmr, color = "BMR", linetype = "BMR")) +
+    geom_segment(aes(x=0, xend=dat$bmd, y = dat$bmr, yend=dat$bmr, color = "BMR", linetype = "BMR")) +
     geom_point() +
     scale_x_continuous(limits = l3_range, trans = "log10") +
-    scale_color_viridis_d("", direction = -1, guide = guide_legend(reverse = TRUE, order = 2)) +
-    scale_linetype_manual("", guide = guide_legend(reverse = TRUE, order = 2), values = c(2, 2, 2, 2, 3, 1)) +
+    scale_color_manual("", guide = guide_legend(reverse = TRUE, order = 2),
+                       values=c("#BBDF27FF","#38B977FF","#38B977FF","#25848EFF","#3E4B89FF","#440154FF")) +
+    #color values from viridis::viridis(5,end=0.9)
+    scale_linetype_manual("", guide = guide_legend(reverse = TRUE, order = 2), values = c(2, 1, 2, 2, 3, 1)) +
     xlab(paste0("Concentration ", "(", dat$conc_unit, ")")) +
     ylab(stringr::str_to_title(gsub("_", " ", dat$normalized_data_type))) +
     geom_text(data = annotations, aes(x = xpos, y = ypos, hjust = hjustvar, vjust = vjustvar, label = annotateText)) +
