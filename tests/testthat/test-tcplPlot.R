3664572b9b73b6d002fce8aed844f2f94512c368
Dunham <Dunham.Kurt@epa.gov>
2023-04-11 16:43:04 -0400

    added test-tcplPlot.R unit tests

--- tests/testthat/test-tcplPlot.R
+++ tests/testthat/test-tcplPlot.R
@@ -5,6 +5,43 @@ test_that("round_n function works", {
     c("1.2e+03",NA,"0.333"))
   })
 
+test_that("check_tcpl_db_schema() loaded", {
+  expect_false(
+    (check_tcpl_db_schema())
+  )
+})
+
+test_that("tcplPlot loads data", {
+  input <- tcplLoadData(lvl = 4, type = "mc")
+  expect_no_error(
+    if (nrow(input) == 0) stop("No data for fld/val provided")
+  )
+  input <- NULL  
+  expect_error(
+    if (nrow(input) == 0) stop("No data for fld/val provided")
+  ) 
+})
+
+test_that("logic check works: assign multi=TRUE for output='pdf'", {
+  output <- "pdf"
+  multi  <-  NULL
+  expect_true(
+  if (output == "pdf" && is.null(multi)) {
+    multi <- TRUE
+  }, multi)
+})
+
+test_that("logic check works: assign multi=FALSE for output != 'pdf', 
+                              verbose=FALSE for output='console'", {
+  output = 'console'
+  expect_false(
+    if (output !="pdf") {
+      multi <- FALSE
+      if(output =="console"){
+        verbose <- FALSE
+        }
+      }, c(multi,verbose))
+    })
 
 # test_that("description", {
 #   expect_*(code)
