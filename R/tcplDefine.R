75ee28a92399ea20b567db4e7bec1cd70d26172f
Thunes <Thunes.Carter@epa.gov>
2023-06-12 10:49:02 -0400

    64 remove field param from tcplDefine

--- R/tcplDefine.R
+++ R/tcplDefine.R
@@ -8,17 +8,14 @@
 #' \code{tcplDefine} queries the tcpl databases and returns field descriptions 
 #' from the data dictionary.
 #' 
-#' @param field Character of length 1, \code{'invitrodb_table'} or 
-#' \code{'invitrodb_field'}, whether to search by table name (invitrodb_table), 
-#' or field name (invitrodb_field)
-#' @param val The values to query on
+#' @param val The values to query on. Can be any combination of table names
+#' (to return all of its field descriptions) and field names
 #' 
 #' @details
 #' Short descriptions of fields for different tables are stored in a data
-#' dictionary. Query by \code{'invitrodb_table'} to retrieve descriptions of 
-#' each field in the given table, or query by \code{'invitrodb_field'} to
-#' retrieve descriptions on every field with the given name, regardless of
-#' which table they are in.
+#' dictionary. Query by table name to retrieve descriptions of each field in
+#' the given table, and/or query by field name to retrieve descriptions on
+#' every field with the given name, regardless of which table they belong to.
 #' 
 #' @examples 
 #' ## Store the current config settings, so they can be reloaded at the end 
@@ -30,13 +27,18 @@
 #' ## dictionary
 #' tcplDefine()
 #' 
-#' ## Specifying invitrodb tables of 'chemical' and 'sample' yields all of the
+#' ## Specifying table names of 'chemical' and 'sample' yields all of the
 #' ## fields from the 'chemical' and 'sample' tables
-#' tcplDefine(field="invitrodb_table", val=c("chemical", "sample"))
+#' tcplDefine(val=c("chemical", "sample"))
 #' 
-#' ## Specifying an invitrodb field of 'wllt' yields all of the fields from any
-#' ## table that contains 'wllt' as a field
-#' tcplDefine(field="invitrodb_field", val=c("wllt"))
+#' ## Specifying a field of 'wllt' yields all of the fields from any table that
+#' ## contains 'wllt' as a field
+#' tcplDefine(val=c("wllt"))
+#' 
+#' ## Specifying a combination of table and field names results in all of the
+#' ## fields which are contained in the given tables and all of the given fields
+#' ## found in any table
+#' tcplDefine(c("chemical", "spid", "wllt"))
 #' 
 #' ## Reset configuration
 #' options(conf_store)
@@ -47,19 +49,17 @@
 #' @import data.table
 #' @export
 
-tcplDefine <- function(field = NULL, val = NULL) {
+tcplDefine <- function(val = NULL) {
   
   tbl = c("invitrodb_dd")
   
   qformat <- "SELECT invitrodb_table, invitrodb_field, description FROM invitrodb_dd"
   
-  if (!is.null(field)) {
+  if (!is.null(val)) {
     
-    vfield <- c("invitrodb_table", "invitrodb_field")
-    if (!field %in% vfield) stop("Invalid 'field' value.")
-    
-    qformat <- paste(qformat, "WHERE %s IN (%s);")
-    qstring <- sprintf(qformat, field, paste0("\"", val, "\"", collapse = ","))
+    vstring <- paste0("\"", val, "\"", collapse = ",")
+    qformat <- paste(qformat, "WHERE invitrodb_table IN (%s) OR invitrodb_field IN (%s);")
+    qstring <- sprintf(qformat, vstring, vstring)
     
   } else {
     
@@ -69,10 +69,10 @@ tcplDefine <- function(field = NULL, val = NULL) {
   
   dat <- tcplQuery(query = qstring, db = getOption("TCPL_DB"), tbl=tbl)
   
-  if (!is.null(field)) {
+  if (!is.null(val)) {
     
     if (nrow(dat) == 0) {
-      warning("The given ", field, "(s) do not exist.")
+      warning("The given table and/or field names were not found.")
       return(dat[])
     }
     
