c00b89553041739dd39c0bb28f034c6097cf2752
cthunes <thunes.carter@epa.gov>
2023-06-13 16:21:07 -0400

    64 modified tcplDefine to allow example data use

--- R/tcplDefine.R
+++ R/tcplDefine.R
@@ -21,7 +21,7 @@
 #' ## Store the current config settings, so they can be reloaded at the end 
 #' ## of the examples
 #' conf_store <- tcplConfList()
-#' tcplConfExample()
+#' tcplConf(drvr = "example")
 #' 
 #' ## Passing no parameters returns all of the fields described in the data
 #' ## dictionary
@@ -29,11 +29,11 @@
 #' 
 #' ## Specifying table names of 'chemical' and 'sample' yields all of the
 #' ## fields from the 'chemical' and 'sample' tables
-#' tcplDefine(val=c("chemical", "sample"))
+#' tcplDefine(c("chemical", "sample"))
 #' 
 #' ## Specifying a field of 'wllt' yields all of the fields from any table that
 #' ## contains 'wllt' as a field
-#' tcplDefine(val=c("wllt"))
+#' tcplDefine("wllt")
 #' 
 #' ## Specifying a combination of table and field names results in all of the
 #' ## fields which are contained in the given tables and all of the given fields
@@ -51,34 +51,57 @@
 
 tcplDefine <- function(val = NULL) {
   
-  tbl = c("invitrodb_dd")
+  drvr <- getOption("TCPL_DRVR")
   
-  qformat <- "SELECT invitrodb_table, invitrodb_field, description FROM invitrodb_dd"
-  
-  if (!is.null(val)) {
+  # example driver
+  if (drvr == "example"){
     
-    vstring <- paste0("\"", val, "\"", collapse = ",")
-    qformat <- paste(qformat, "WHERE invitrodb_table IN (%s) OR invitrodb_field IN (%s);")
-    qstring <- sprintf(qformat, vstring, vstring)
+    dd <- invitrodb_dd[]
     
-  } else {
-    
-    qstring <- paste0(qformat, ";")
-    
-  }
-  
-  dat <- tcplQuery(query = qstring, db = getOption("TCPL_DB"), tbl=tbl)
-  
-  if (!is.null(val)) {
-    
-    if (nrow(dat) == 0) {
-      warning("The given table and/or field names were not found.")
-      return(dat[])
+    if (!is.null(val)) {
+      dd <- dd[dd$invitrodb_table %in% val | dd$invitrodb_field %in% val,]
+      
+      if (nrow(dd) == 0) {
+        warning("The given table and/or field names were not found.")
+      }
     }
     
+    return(dd)
+    
   }
-  
-  dat[]
+  # MySQL driver
+  if (drvr != "example") { 
+    
+    tbl = c("invitrodb_dd")
+    
+    qformat <- "SELECT invitrodb_table, invitrodb_field, description FROM invitrodb_dd"
+    
+    if (!is.null(val)) {
+      
+      vstring <- paste0("\"", val, "\"", collapse = ",")
+      qformat <- paste(qformat, "WHERE invitrodb_table IN (%s) OR invitrodb_field IN (%s);")
+      qstring <- sprintf(qformat, vstring, vstring)
+      
+    } else {
+      
+      qstring <- paste0(qformat, ";")
+      
+    }
+    
+    dat <- tcplQuery(query = qstring, db = getOption("TCPL_DB"), tbl=tbl)
+    
+    if (!is.null(val)) {
+      
+      if (nrow(dat) == 0) {
+        warning("The given table and/or field names were not found.")
+        return(dat[])
+      }
+      
+    }
+    
+    dat[]
+    
+  }
   
 }
 
