4a5fde58bd5e584e50000f9f208bf949ecf6c90b
cthunes <thunes.carter@epa.gov>
2023-08-02 16:02:33 -0400

    106 created bmad5.onesd16.static mc4 method

--- R/mc4_mthds.R
+++ R/mc4_mthds.R
@@ -46,6 +46,10 @@
 #'     required for tcplfit2 processing.}
 #'     \item{bidirectional.false}{Limits bidirectional fitting and processes data in positive 
 #'     analysis direction only. Use for gain-of-signal or inverted data.}
+#'     \item{bmad5.onesd16.static}{Replace bmad (baseline median absolute deviation) with 5 and osd
+#'     (one standard deviation of the normalized response for test compound wells (wllt = t) with a
+#'     concentration index (cndx) of 1 or 2) with 16. Typically used for binary data where values
+#'     would otherwise be 0 since non-zero values are required for tcplfit2 processing.}
 #'   }
 #' 
 #' @note
@@ -75,9 +79,18 @@ mc4_mthds <- function() {
       list(e1)
       
     },
+    
     bidirectional.false = function() {
       
       e1 <- bquote(dat[ ,bidirectional := FALSE])
+      list(e1)
+      
+    },
+    
+    bmad5.onesd16.static = function() {
+      
+      e1 <- bquote(dat[ , bmad := 5])
+      e2 <- bquote(dat[ , osd := 16])
       list(e1)
       
     }
