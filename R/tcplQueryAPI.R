1d219d92eddd1060dd4311c31d911890eaaa7f47
cthunes <thunes.carter@epa.gov>
2024-03-20 18:33:52 -0400

    202 reworked tcplQueryAPI to pull from all assays or just by aeid
    pulls from all assays if supplied 'fld' is not 'aeid'

--- R/tcplQueryAPI.R
+++ R/tcplQueryAPI.R
@@ -21,23 +21,38 @@ tcplQueryAPI <- function(resource = "data", fld = NULL, val = NULL, return_flds 
     dat <- exec(get_bioactivity_details_batch, !!sym(fld) := val, Server := paste0(getOption("TCPL_HOST"), "/data"))
     dat <- do.call(rbind, dat)
     
-    # adjust column names
-    colnames(dat) <- gsub("([a-z])([A-Z])", "\\1_\\L\\2", colnames(dat), perl = TRUE)
     dat$dsstox_substance_id <- dat$dtxsid
     
     # unlist logc to conc
     dat <- dat |> rowwise() |> mutate(conc = list(10^unlist(logc)))
     
-    if (is.null(return_flds)) return(dat)
-    else return(dat[, ..return_flds])
-    
   } else if (resource == "assay") {
     
     # check fld
     if (is.null(fld)) fld == "AEID"
-    fld <- toupper(fld)
-    if (fld != "AEID") stop("'fld' must be 'AEID' if resource is 'assay'")
+    
+    # get data from API using ccdR
+    if (toupper(fld) == "AEID") { 
+      # using get annotations by aeid
+      dat <- get_annotation_by_aeid_batch(AEID = val, Server = paste0(getOption("TCPL_HOST")))
+      dat <- do.call(rbind, dat)
+    } else { 
+      # using get all assays and then filtering
+      dat <- get_all_assays(Server = paste0(getOption("TCPL_HOST")))
+    }
     
   } else stop("'resource' must = 'data' or 'assay'.")
   
+  # adjust column names
+  colnames(dat) <- gsub("([a-z])([A-Z])", "\\1_\\L\\2", colnames(dat), perl = TRUE)
+  
+  # filter if searching by assay list for non-aeid val
+  if (resource == "assay" && toupper(fld) != "AEID") 
+    dat <- dat[dat[[fld]]== val,]
+  
+  if (is.null(return_flds)) return(dat)
+  else {
+    return_flds <- intersect(c(tolower(fld), return_flds), colnames(dat))
+    return(dat[, return_flds])
+  }
 }
