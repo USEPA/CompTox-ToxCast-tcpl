8108fbc1a1e7c7ad23ff1d698cdcb3822d5f9230
cthunes <thunes.carter@epa.gov>
2024-03-27 19:21:44 -0400

    202 checks, warnings, errors, and multiple field filtering

--- R/tcplQueryAPI.R
+++ R/tcplQueryAPI.R
@@ -10,49 +10,65 @@
 
 tcplQueryAPI <- function(resource = "data", fld = NULL, val = NULL, return_flds = NULL) {
   
+  if (getOption("TCPL_DRVR") != "API") stop("TCPL_DRVR must be set to 'API'. See ?tcplConf.")
+  
   if (resource == "data") {
     
     # check fld
     if (is.null(fld)) stop("'fld' cannot be NULL")
+    if (length(fld) > 1) stop("'fld' must be length 1")
     fld <- if(tolower(fld) == "m4id") tolower(fld) else toupper(fld)
-    if (!(fld %in% c("AEID", "SPID", "m4id", "DTXSID"))) stop("'fld' must be one of 'AEID', 'SPID', 'm4id', or 'DTXSID'")
+    if (!(fld %in% c("AEID", "SPID", "m4id", "DTXSID"))) 
+      stop("'fld' must be one of 'AEID', 'SPID', 'm4id', or 'DTXSID'")
     
     # get data from API using ccdR
-    dat <- exec(get_bioactivity_details_batch, !!sym(fld) := val, Server := paste0(getOption("TCPL_HOST"), "/data"))
+    dat <- suppressMessages(exec(get_bioactivity_details_batch, !!sym(fld) := val, Server := paste0(getOption("TCPL_HOST"), "/data")))
+    
+    # remove missing elements
+    lb <- length(dat) # store length before
+    na_names <- names(dat[is.na(dat)])
+    dat <- dat[!is.na(dat)]
+    if (lb != length(dat)) warning(paste0("Data not found for the following 'fld' and 'val' combos: \n", paste0(fld, ": ", na_names, collapse = "\n")))
+    
     dat <- do.call(rbind, dat)
+    if (is.null(dat)) return(data.table())
     
     dat$dsstox_substance_id <- dat$dtxsid
     
     # unlist logc to conc
-    dat <- dat |> rowwise() |> mutate(conc = list(10^unlist(logc)))
+    dat <- dat |> rowwise() |> mutate(conc = list(10^unlist(logc))) %>% as.data.table()
     
   } else if (resource == "assay") {
     
-    # check fld
-    if (is.null(fld)) fld == "AEID"
-    
-    # get data from API using ccdR
-    if (toupper(fld) == "AEID") { 
-      # using get annotations by aeid
-      dat <- get_annotation_by_aeid_batch(AEID = val, Server = paste0(getOption("TCPL_HOST")))
-      dat <- do.call(rbind, dat)
-    } else { 
-      # using get all assays and then filtering
-      dat <- get_all_assays(Server = paste0(getOption("TCPL_HOST")))
-    }
+    # using get all assays and then filtering
+    dat <- get_all_assays(Server = paste0(getOption("TCPL_HOST"))) %>% as.data.table()
     
   } else stop("'resource' must = 'data' or 'assay'.")
   
   # adjust column names
   colnames(dat) <- gsub("([a-z])([A-Z])", "\\1_\\L\\2", colnames(dat), perl = TRUE)
   
-  # filter if searching by assay list for non-aeid val
-  if (resource == "assay" && toupper(fld) != "AEID") 
-    dat <- dat[dat[[fld]]== val,]
+  # filter if searching by assay list
+  if (resource == "assay" && !is.null(fld)) {
+    
+    if (any(!(fld %in% colnames(dat)))) 
+      stop(paste0("Query field(s) '", 
+                  paste0(fld[which(!(fld %in% colnames(dat)))], collapse = "', '"), 
+                  "' not available. Try using from the following: \n", 
+                  paste0(colnames(dat), collapse = "\n")))
+    
+    fld_indices <- if (length(fld) == 1 | length(fld) == length(val)) 1:length(fld) 
+    else stop("'fld' and 'val' must be the same size if length(fld) > 1")
+    
+    for (i in fld_indices) {
+      dat <- dat[dat[[fld[i]]] %in% val[[i]],]
+    }
+    
+  }
   
   if (is.null(return_flds)) return(dat)
   else {
     return_flds <- intersect(c(tolower(fld), return_flds), colnames(dat))
-    return(dat[, return_flds])
+    return(dat[, ..return_flds])
   }
 }
