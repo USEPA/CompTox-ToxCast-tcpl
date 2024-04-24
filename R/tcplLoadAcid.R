7155f7a8ca328285fdbfddd4f528e1618d7aeafa
cthunes <thunes.carter@epa.gov>
2024-04-24 19:13:04 -0400

    233 integrated tcplQueryAPI into 4 tcplLoadAxxx funcs

--- R/tcplLoadAcid.R
+++ R/tcplLoadAcid.R
@@ -7,6 +7,14 @@
 #' @export
 
 tcplLoadAcid <- function(fld = NULL, val = NULL, add.fld = NULL) {
+  
+  if (getOption("TCPL_DRVR") == "API") {
+    dat <- tcplQueryAPI(resource = "assay", fld = fld, val = val, return_flds = c("acid", "assay_component_name", add.fld))
+    setnames(dat, "assay_component_name", "acnm")
+    setorder(dat, "acid")
+    return(unique(dat, by = c(fld, "acid", "acnm")))
+  }
+  
   tbl = c("assay_component", "assay_component_map", "assay")
   out <- c("assay_component.acid", 
            "assay_component.assay_component_name")
--- R/tcplLoadAsid.R
+++ R/tcplLoadAsid.R
@@ -7,6 +7,14 @@
 #' @export
 
 tcplLoadAsid <- function(fld = NULL, val = NULL, add.fld = NULL) {
+  
+  if (getOption("TCPL_DRVR") == "API") {
+    dat <- tcplQueryAPI(resource = "assay", fld = fld, val = val, return_flds = c("asid", "assay_source_name", add.fld))
+    setnames(dat, "assay_source_name", "asnm")
+    setorder(dat, "asid")
+    return(unique(dat, by = c(fld, "asid", "asnm")))
+  }
+  
   tbl = "assay_source"
   out <- c("assay_source.asid", 
            "assay_source.assay_source_name")
