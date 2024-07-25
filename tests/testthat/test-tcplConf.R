d2c2586c90cc72454abb03a1252d000ccdef0424
Carter Thunes <Thunes.Carter@epa.gov>
2024-07-25 15:27:32 -0400

    198 update tcplConf related tests, add API tests

--- tests/testthat/test-tcplConf.R
+++ tests/testthat/test-tcplConf.R
@@ -8,6 +8,21 @@ test_that("unsupported driver error message", {
   drvr  <-  "test"
   expect_error(
     tcplConf(drvr),
-    paste0(drvr," is not a supported database driver. Must be 'MySQL', 'tcplLite', or 'example'.")
+    paste0(drvr," is not a supported database driver. Must be 'MySQL', 'tcplLite', 'API' or 'example'.")
 )
+})
+
+test_that("API driver no key error message", {
+  drvr  <-  "API"
+  expect_error(
+    tcplConf(drvr),
+    "'API' driver requires an API-key, supply it to the 'pass' parameter. To request a key, send an email to ccte_api@epa.gov."
+  )
+})
+
+test_that("API driver properly sets URL", {
+  tcplConf(drvr = "API", pass = "api_key")
+  expect_equal("https://api-ccte.epa.gov/bioactivity",
+               getOption("TCPL_HOST")
+  )
 })
