7bbb2b59de73866708adff03e5398dee907622b3
Carter Thunes <Thunes.Carter@epa.gov>
2024-08-21 17:32:42 -0400

    259 fix test after removal of 'tcplLite' in error message

--- tests/testthat/test-tcplConf.R
+++ tests/testthat/test-tcplConf.R
@@ -9,7 +9,7 @@ test_that("unsupported driver error message", {
   drvr  <-  "test"
   expect_error(
     tcplConf(drvr),
-    paste0(drvr," is not a supported database driver. Must be 'MySQL', 'tcplLite', 'API' or 'example'.")
+    paste0(drvr," is not a supported database driver. Must be 'MySQL', 'API', or 'example'.")
 )
 })
 
