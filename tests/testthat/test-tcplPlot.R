4812a9a2dd104b31547e0d246659f0514a355e93
Carter Thunes <Thunes.Carter@epa.gov>
2024-12-09 12:00:02 -0500

    add ggplot output test cases

--- tests/testthat/test-tcplPlot.R
+++ tests/testthat/test-tcplPlot.R
@@ -746,6 +746,109 @@ test_that("standalone advanced comparison plotting works in sc", {
   file.remove(fn) # clean up
 })
 
+# "ggplot" output tests
+test_that("ggplot output works in mc", {
+  data("mc_test")
+  mocked <- mc_test$plot_single_m4id
+  local_mocked_bindings(
+    tcplQuery = function(query, db, tbl) {
+      if (query == "SHOW VARIABLES LIKE 'max_allowed_packet'") mc_test$tcplConfQuery
+      else mocked[query][[1]]
+    }
+  )
+  tcplConf(drvr = "MySQL", db = "invitrodb") # must include both
+  expect_no_error(suppressWarnings(otpt <- tcplPlot(type = "mc", fld = "m4id", val = mocked$m4id, output = "ggplot", flags = TRUE, fileprefix = "temp_tcplPlot")))
+  expect_true(ggplot2::is.ggplot(otpt))
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 0) # no files should be generated
+  expect_no_error(suppressWarnings(otpt <- tcplPlot(type = "mc", fld = "m4id", val = mocked$m4id, output = "pdf", flags = TRUE, fileprefix = "temp_tcplPlot")))
+  expect_true(ggplot2::is.ggplot(otpt)) # single outputs now return ggplot as well
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 1) # exactly one file created
+  file.remove(fn) # clean up
+})
+
+test_that("ggplot output comparison plotting works in mc", {
+  data("mc_test")
+  mocked <- mc_test$plot_single_m4id_compare
+  local_mocked_bindings(
+    tcplQuery = function(query, db, tbl) {
+      if (query == "SHOW VARIABLES LIKE 'max_allowed_packet'") mc_test$tcplConfQuery
+      else mocked[query][[1]]
+    }
+  )
+  tcplConf(drvr = "MySQL", db = "invitrodb") # must include both
+  expect_no_error(suppressWarnings(otpt <- tcplPlot(type = "mc", fld = "m4id", val = mocked$m4id, compare.val = mocked$compare.m4id, output = "ggplot", flags = TRUE, fileprefix = "temp_tcplPlot")))
+  expect_true(ggplot2::is.ggplot(otpt))
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 0) # no files should be generated
+  expect_no_error(suppressWarnings(otpt <- tcplPlot(type = "mc", fld = "m4id", val = mocked$m4id, compare.val = mocked$compare.m4id, output = "pdf", flags = TRUE, fileprefix = "temp_tcplPlot")))
+  expect_true(ggplot2::is.ggplot(otpt)) # single outputs now return ggplot as well
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 1) # exactly one file created
+  file.remove(fn) # clean up
+})
+
+test_that("ggplot output works in sc", {
+  data("sc_test")
+  mocked <- sc_test$plot_single_s2id
+  local_mocked_bindings(
+    tcplQuery = function(query, db, tbl) {
+      if (query == "SHOW VARIABLES LIKE 'max_allowed_packet'") sc_test$tcplConfQuery
+      else mocked[query][[1]]
+    }
+  )
+  tcplConf(drvr = "MySQL", db = "invitrodb") # must include both
+  expect_no_error(suppressWarnings(otpt <- tcplPlot(type = "sc", fld = "s2id", val = mocked$s2id, output = "ggplot", fileprefix = "temp_tcplPlot")))
+  expect_true(ggplot2::is.ggplot(otpt))
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 0) # no files should be generated
+  expect_no_error(suppressWarnings(otpt <- tcplPlot(type = "sc", fld = "s2id", val = mocked$s2id, output = "pdf", fileprefix = "temp_tcplPlot")))
+  expect_true(ggplot2::is.ggplot(otpt)) # single outputs now return ggplot as well
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 1) # exactly one file created
+  file.remove(fn) # clean up
+})
+
+test_that("ggplot output comparison plotting works in sc", {
+  data("sc_test")
+  mocked <- sc_test$plot_single_s2id_compare
+  local_mocked_bindings(
+    tcplQuery = function(query, db, tbl) {
+      if (query == "SHOW VARIABLES LIKE 'max_allowed_packet'") sc_test$tcplConfQuery
+      else mocked[query][[1]]
+    }
+  )
+  tcplConf(drvr = "MySQL", db = "invitrodb") # must include both
+  expect_no_error(suppressWarnings(otpt <- tcplPlot(type = "sc", fld = "s2id", val = mocked$s2id, compare.val = mocked$compare.s2id, output = "ggplot", fileprefix = "temp_tcplPlot")))
+  expect_true(ggplot2::is.ggplot(otpt))
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 0) # no files should be generated
+  expect_no_error(suppressWarnings(otpt <- tcplPlot(type = "sc", fld = "s2id", val = mocked$s2id, compare.val = mocked$compare.s2id, output = "pdf", fileprefix = "temp_tcplPlot")))
+  expect_true(ggplot2::is.ggplot(otpt)) # single outputs now return ggplot as well
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 1) # exactly one file created
+  file.remove(fn) # clean up
+})
+
+test_that("ggplot output returns specific warnings and errors", {
+  data("mc_test")
+  mocked <- mc_test$plot_multiple_m4id
+  local_mocked_bindings(
+    tcplQuery = function(query, db, tbl) {
+      if (query == "SHOW VARIABLES LIKE 'max_allowed_packet'") mc_test$tcplConfQuery
+      else mocked[query][[1]]
+    }
+  )
+  tcplConf(drvr = "MySQL", db = "invitrodb") # must include both
+  expect_error(tcplPlot(type = "mc", fld = "m4id", val = mocked$m4id, output = "ggplot", flags = TRUE, multi = TRUE, fileprefix = "temp_tcplPlot"),
+               "More than 1 concentration series returned for given field/val combination. Set output to pdf or reduce the number of plots to 1. Current number of individual plots: 2")
+  mocked <- mc_test$plot_single_m4id
+  expect_message(suppressWarnings(tcplPlot(type = "mc", fld = "m4id", val = mocked$m4id, output = "ggplot", verbose = TRUE, flags = TRUE, multi = TRUE, fileprefix = "temp_tcplPlot")),
+                 "ggplot object and verbose table arranged into gtable object. To work with a simple ggplot object, set `verbose = FALSE`.")
+  fn <- stringr::str_subset(list.files(), "^temp_tcplPlot")
+  expect_length(fn, 0) # no files should be generated
+})
 
 
 #-------------------------------------------------------------------------------
