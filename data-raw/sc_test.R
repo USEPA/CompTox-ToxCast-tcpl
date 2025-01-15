47a3e91ac55f862c0416976bd06e7f205d706a1c
Carter Thunes <Thunes.Carter@epa.gov>
2025-01-15 17:40:37 -0500

    293 updated data_raw/sc_test.R for refactored tcplPlot

--- data-raw/sc_test.R
+++ data-raw/sc_test.R
@@ -47,16 +47,20 @@ l1_sample2 <- l1[sample(1:nrow(l1),size = 2,replace = FALSE)]
 l2 <- tcplLoadData(type = "sc", lvl = 2, fld = "aeid", val = aeid)
 l2_sample1 <- l2[sample(1:nrow(l2),size = 1,replace = FALSE)]
 l2_sample2 <- l2[sample(1:nrow(l2),size = 2,replace = FALSE)]
-# pick compare.val endpoints and ids
+# pick compare endpoints and ids
 # be sure to only allow to choose from endpoints with the same number of samples
 sc2_counts <- filter(sc2_counts, n == sc2_counts[aeid == selected]$n & aeid != selected)
 compare.aeid <- sc2_counts[sample(1:nrow(sc2_counts),size = 1,replace = FALSE),aeid]
+selected <- c(selected, compare.aeid)
 compare.l2 <- tcplLoadData(type = "sc", lvl = 2, fld = "aeid", val = compare.aeid)
-compare.l2_sample1 <- compare.l2[sample(1:nrow(compare.l2),size = 1,replace = FALSE)]
-compare.l2_sample2 <- compare.l2[sample(1:nrow(compare.l2),size = 2,replace = FALSE)]
+compare.l2_sample1 <- compare.l2[spid %in% l2_sample1$spid]
+compare.l2_sample2 <- compare.l2[spid %in% l2_sample2$spid]
+# pick extra aeids for multiple plot
+sc2_counts <- filter(sc2_counts, n == unique(sc2_counts[aeid %in% selected]$n) & !aeid %in% selected)
+extra.aeids <- sc2_counts[sample(1:nrow(sc2_counts),size = 2,replace = FALSE),aeid]
 
 
-get_query_data <- function(lvl, fld, val, compare.val = NULL, add.fld = TRUE, func = "tcplLoadData") {
+get_query_data <- function(lvl, fld, val, compare = "m4id", add.fld = TRUE, func = "tcplLoadData") {
   
   # IMPORTANT || MUST ADD TEMPORARY LINE TO TCPLQUERY --------------------------
   # add temporary line to top of tcplQuery to get the query string: print(query)
@@ -64,7 +68,7 @@ get_query_data <- function(lvl, fld, val, compare.val = NULL, add.fld = TRUE, fu
     query_strings <- capture.output(result<-tcplLoadData(type = "sc", lvl = lvl, fld = fld, val = val, add.fld = add.fld))
   } else if (func == "tcplPlot") {
     query_strings <- capture.output(result<-tcplPlot(type = "sc", fld = fld, 
-                                                     val = val, compare.val = compare.val, 
+                                                     val = val, compare = compare, 
                                                      output = "pdf", multi = TRUE, verbose = TRUE,
                                                      fileprefix = "temp_tcplPlot"))
     file.remove(stringr::str_subset(list.files(), "^temp_tcplPlot")) # clean up
@@ -80,7 +84,6 @@ get_query_data <- function(lvl, fld, val, compare.val = NULL, add.fld = TRUE, fu
   
   # also store fld and val in list object for use in test case
   dat[fld] <- val
-  if (!is.null(compare.val)) dat[sprintf("compare.%s", fld)] <- compare.val
   return(dat)
   
 }
@@ -116,28 +119,27 @@ sc_test <- list(
                                       val = list(l2_sample2$spid, aeid), 
                                       func = "tcplPlot"),
   plot_single_s2id_compare = get_query_data(fld = "s2id", 
-                                            val = l2_sample1$s2id, 
-                                            compare.val = compare.l2_sample1$s2id, 
+                                            val = list(c(l2_sample1$s2id, compare.l2_sample1$s2id)), 
+                                            compare = "spid", 
                                             func = "tcplPlot"),
   plot_multiple_s2id_compare = get_query_data(fld = "s2id", 
-                                              val = list(l2_sample2$s2id), 
-                                              compare.val = list(compare.l2_sample2$s2id), 
+                                              val = list(c(l2_sample2$s2id, compare.l2_sample2$s2id)), 
+                                              compare = "spid", 
                                               func = "tcplPlot"),
   plot_single_aeid_compare = get_query_data(fld = "aeid", 
-                                            val = aeid, 
-                                            compare.val = compare.aeid, 
+                                            val = list(c(aeid, compare.aeid)),
+                                            compare = "spid",  
                                             func = "tcplPlot"),
   plot_multiple_aeid_compare = get_query_data(fld = "aeid", 
-                                              val = list(c(aeid, compare.aeid)), 
-                                              compare.val = list(c(compare.aeid, aeid)), 
+                                              val = list(c(aeid, compare.aeid, extra.aeids)), 
+                                              compare = "spid",
                                               func = "tcplPlot"),
   plot_single_spid_compare = get_query_data(fld = c("spid", "aeid"), 
-                                            val = list(l2_sample1$spid, aeid), 
-                                            compare.val = list(compare.l2_sample1$spid, compare.aeid), 
+                                            val = list(c(l2_sample1$spid, compare.l2_sample1$spid), c(aeid, compare.aeid)),
+                                            compare = "spid",
                                             func = "tcplPlot"),
   plot_multiple_spid_compare = get_query_data(fld = c("spid", "aeid"), 
-                                              val = list(l2_sample2$spid, aeid), 
-                                              compare.val = list(compare.l2_sample2$spid, compare.aeid), 
+                                              val = list(c(l2_sample2$spid, compare.l2_sample2$spid), c(aeid, compare.aeid)),
                                               func = "tcplPlot")
 )
 #---------------------------#
