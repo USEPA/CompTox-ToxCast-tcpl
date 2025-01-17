cd47876c7fda61a8d0919e1de9e373f31a47b9ab
Carter Thunes <Thunes.Carter@epa.gov>
2025-01-17 15:56:18 -0500

    293 create new files for testing tcplggplot2.R and tcplggplot2Utils.R

---
+++ tests/testthat/test-tcplggplot2Utils.R
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# tests using drvr = "MySQL" and mocking
+#-------------------------------------------------------------------------------
+# Format
+# test_that("something works", {
+# data("mc_test")
+# mocked <- mc_test$mc0_by_m0id # change this
+# local_mocked_bindings(
+#   tcplQuery = function(query, db, tbl) {
+#     if (query == "SHOW VARIABLES LIKE 'max_allowed_packet'") mc_test$tcplConfQuery
+#     else mocked[query][[1]]
+#   }
+# )
+# tcplConf(drvr = "MySQL", db = "invitrodb") # must include both
+# })
