e77019b24f1cd9cf2bc1d12828b78bc2ff7802a8
Carter Thunes <Thunes.Carter@epa.gov>
2025-01-22 20:09:34 -0500

    293 update mc_test with missing chem test case for new LoadWllt test

--- tests/testthat/test-tcplPlotUtils.R
+++ tests/testthat/test-tcplPlotUtils.R
@@ -176,3 +176,29 @@ test_that("tcplPlotSetYRange correctly sets yrange for sc data", {
   max <- max(max, dat$coff, dat$coff * -1)
   expect_equal(yrange, c(min,max))
 })
+
+
+#-------------------------------------------------------------------------------
+# Test tcplPlotLoadWllt
+#-------------------------------------------------------------------------------
+test_that("tcplPlotLoadWllt replaces NA dtxsid and chnm with well type description", {
+  data("mc_test")
+  mocked <- mc_test$plot_single_aeid_missing_chem
+  local_mocked_bindings(
+    tcplQuery = function(query, db, tbl) {
+      if (query == "SHOW VARIABLES LIKE 'max_allowed_packet'") mc_test$tcplConfQuery
+      else mocked[query][[1]]
+    }
+  )
+  tcplConf(drvr = "MySQL", db = "invitrodb") # must include both
+  dat <- tcplPlotLoadData(fld = "aeid", val = mocked$aeid, flags = TRUE)
+  dat[is.na(code), c("chnm", "dsstox_substance_id") := NA]
+  dat$wllt_desc = NULL
+  expect_true(nrow(dat[is.na(chnm) | is.na(dsstox_substance_id)]) != 0)
+  dat <- tcplPlotLoadWllt(dat = dat)
+  expect_equal(nrow(dat[is.na(chnm) | is.na(dsstox_substance_id)]), 0)
+  expect_true(any(stringr::str_detect(dat$wllt_desc[!is.na(dat$wllt_desc)][1],
+                                      c("Gain-of-signal control", "Loss-of-signal control", 
+                                        "Neutral/negative ", "Blank", "Viability control", 
+                                        "Well type: "))))
+})
