8f19911a30e3c23f97aec68feeb987deb24f0bc2
Carter Thunes <Thunes.Carter@epa.gov>
2025-02-05 13:12:18 -0500

    314 update tcplLoadChem/tests for bug fix involving missing spid col

--- tests/testthat/test-tcplLoadChem.R
+++ tests/testthat/test-tcplLoadChem.R
@@ -18,6 +18,12 @@ httptest::with_mock_dir("ctx", {
     expect_true(all(c("spid", "chid", "casn", "chnm", "dsstox_substance_id", "code") %in% colnames(dat)))
     expect_equal(dat$spid, test_api$spid)
   })
+  test_that("include.spid = FALSE works", {
+    expect_no_error(dat <- tcplLoadChem(field = "spid", val = test_api$spid, include.spid = FALSE))
+    expect_true(is.data.table(dat))
+    expect_equal(nrow(dat), 1)
+    expect_true(all(c("chid", "casn", "chnm", "dsstox_substance_id", "code") %in% colnames(dat)))
+  })
   test_that("exact is silently changed to TRUE if driver is API", {
     expect_no_error(dat <- tcplLoadChem(field = "spid", val = test_api$spid, exact = FALSE))
     expect_true(is.data.table(dat))
@@ -27,6 +33,7 @@ httptest::with_mock_dir("ctx", {
   })
   #error cases
   test_that("field can only be spid if driver is API", {
+    expect_error(tcplLoadChem(), "When drvr option is set to 'API', only 'spid' is a valid 'field' value.")
     expect_error(tcplLoadChem(field = "chnm", val = "fakechnm"), "When drvr option is set to 'API', only 'spid' is a valid 'field' value.")
   })
   test_that("data not found results in null data.table", {
